9a8b436538990f9d9db8031e0a3c86074456381c
geteway <Liangzxdu@foxmail.com>
2015-08-04 22:16:46 +0930

    add
    add new function

--- NaiveBayes/NaiveBayes1.py
+++ NaiveBayes/NaiveBayes1.py
@@ -10,16 +10,42 @@ Introduction : This program is about the NaiveBayes algorithm for a simple examp
                You can find more information about this program in 'NaiveBayes1_doc'(also in this project)
                And this program is based on Numpy
 """
+import numpy
+import time
+
+
 
 def Read_Data () :
     """
     This function is read data from data1.txt then return it
-    :return:
+    :return: A 2-D matrix with the type of numpy.ndarray
     """
     file_read = open('data1.txt')
     L = []
     line = file_read.readline().split()
-    
-    print line,type(line)
+    while line :  # always read until the file is empty.
+        L.append([int(key) for key in line  ])
+        line = file_read.readline().split()
+    return numpy.array(L)  # change from list to 2D matrix and return it.
 
-Read_Data()
+
+def Count_Data ( Data ) :
+    """
+    This function is used to count the diffient member in x1,x2 and y
+    then return C_Y, C_X1, C_X2
+    :return:Three int numbers:
+            C_Y  is the count of different member of Y
+            C_X1 is the count of different member of x1
+            C_X2 is the count of different member of x2
+    """
+    #print type(numpy.array([numpy.bincount( Data[:,-1] )]))
+    #print numpy.array([numpy.bincount( Data[:,-1] )]).shape
+    C_Y  = numpy.array([numpy.bincount( Data[:,-1] )]).shape[1]
+    C_X1 = numpy.array([numpy.bincount( Data[:, 0] )]).shape[1]
+    C_X2 = numpy.array([numpy.bincount( Data[:, 1] )]).shape[1]
+    return C_Y,C_X1,C_X2
+
+Data = Read_Data()
+print Data,type(Data)
+Count_Y, Count_X1, Count_X2 = Count_Data( Data )
+print Count_Y,Count_X1,Count_X2
