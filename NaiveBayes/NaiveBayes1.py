7ddb5ab6d17f29c821428707eb6169419782f592
geteway <Liangzxdu@foxmail.com>
2015-08-04 14:47:40 +0930

    change
    change naive bayes program

--- NaiveBayes/NaiveBayes1.py
+++ NaiveBayes/NaiveBayes1.py
@@ -1,5 +1,6 @@
 __author__ = 'geteway'
 """
+Name   : NaiveBayes1.py (A NaiveBayes algorithm for practice)
 Author : LiangZHANG
 Date   : 2015-8-4
 E-mail : liangzxdu@foxmail.com
@@ -7,6 +8,18 @@ E-mail : liangzxdu@foxmail.com
 Introduction : This program is about the NaiveBayes algorithm for a simple example.
                The example is coming from 'data1.txt'(in this project)
                You can find more information about this program in 'NaiveBayes1_doc'(also in this project)
+               And this program is based on Numpy
 """
 
+def Read_Data () :
+    """
+    This function is read data from data1.txt then return it
+    :return:
+    """
+    file_read = open('data1.txt')
+    L = []
+    line = file_read.readline().split()
+    
+    print line,type(line)
 
+Read_Data()
